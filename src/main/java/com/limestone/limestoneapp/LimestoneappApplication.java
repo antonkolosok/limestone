7783ae02d583276d28d4a2278117465dc0a0a0c2
Anton Kolosok <antonkolosok@gmail.com>
2019-06-18 10:05:59 +0300

    Initial commit

---
+++ src/main/java/com/limestone/limestoneapp/LimestoneappApplication.java
@@ -0,0 +1,13 @@
+package com.limestone.limestoneapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimestoneappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimestoneappApplication.class, args);
+	}
+
+}
